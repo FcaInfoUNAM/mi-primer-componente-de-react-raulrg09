8634268e966917598029853a488c07c23c2f5e26
raulrg09 <93933463+raulrg09@users.noreply.github.com>
2025-05-05 19:48:25 +0000

    Mi primer componente de react

--- my-app/src/Grid.jsx
+++ my-app/src/Grid.jsx
@@ -1,3 +1,12 @@
-export function Grid(){
-    /* Escrobe aquí tu código */
+export function Grid({items}){
+    return(
+        <div className="grid">
+            {/*
+            <div class="card"><h2>FCA</h2><p>fca.unam.mx</p></div>
+            <div class="card"><h2>google</h2><p>wwww.google.com</p></div>
+            <div class="card"><h2>Facebook</h2><p>www.facebook.com</p></div>
+            */}            
+            {items.map(e => <div className="card"><h2>{e.title}</h2><p>{e.ref}</p></div>)}
+        </div>
+    )
 }
